7f523063549476b77775124e1c7c48754c7ecded
newlyedward <newlyedward@qq.com>
2017-09-07 09:31:25 +0800

    perf_comp_data func

--- utils.py
+++ utils.py
@@ -114,10 +114,37 @@ def chinese2digits(chinese):
     result = result + hnd_mln
     return result
 
+# ---------------------- scientific calculation--------------------------------
+def perf_comp_data(func_list, data_list, rep=3, number=1):
+    """Function to compare the performance of different functions
+
+    :param func_list: list
+        list with function names as string
+    :param data_list: list
+        list with data set names as strings
+    :param rep: int
+        number of repetitions of the whole comparison
+    :param number: int
+        number of executions for every function
+    :return:
+    """
+    from timeit import repeat
+    res_list = {}
+    for name in enumerate(func_list):
+        stmt = name[1] + '(' + data_list[name[0]] + ')'
+        setup = "from __main__ import " + name[1] + ',' + data_list[name[0]]
+        results = repeat(stmt=stmt, setup=setup, repeat=rep, number=number)
+        res_list[name[1]] = sum(results) / rep
+    res_sort = sorted(res_list.items(),
+                      key=lambda x: x[1])
+    for item in res_sort:
+        rel = item[1] / res_sort[0][1]
+        print('function:' + item[0] + ', av. time sec: %9.5f, ' % item[1] + 'relative: %6.1f' % rel)
+
 
 if __name__ == "__main__":
     # import doctest
     # doctest.testmod()
 
-    path_name = r"K:\Photos"
+    path_name = r"K:\Photos\unhandle"
     delete_dir(path_name)
